da3d3ff1808a8c64e703275803272896ec7f38a6
rocky <rb@dustyfeet.com>
2015-11-20 13:47:48 -0500

    Beef up description.

--- lib/Devel/Trepan/Deparse.pm
+++ lib/Devel/Trepan/Deparse.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # Copyright (C) 2015 Rocky Bernstein <rocky@cpan.org>
 package Devel::Trepan::Deparse;
-our $VERSION='2.1.0';
+our $VERSION='2.2.0';
 "All of the real action is in Devel::Trepan::CmdProcessor::Command::Deparse.pm";
 __END__
 
@@ -26,10 +26,45 @@ Perl Deparse plugin for L<Devel::Trepan> via L<B::DeparseTree>
 =head1 SUMMARY
 
 This adds I<deparse> and I<deval> commands to the L<Devel::Trepan>
-debugger; I<deparse> deparses Perl code; I<deval> evaluates deparsed
-perl at the current point.
+debugger; I<deparse> deparses Perl code; I<deval> evaluates de-parsed
+Perl at the current point in the Perl program that you are stopped at.
 
 =head1 DESCRIPTION
+
+Perl reports location only at the granularity of a line number. Sometime you would like better or more precise information. For example suppose I am stopped on this line taken from I<File::Basename::fileparse>:
+
+     if (grep { $type eq $_ } qw(MSDOS DOS MSWin32 Epoc)) {  # ...
+
+In a debugger, there happen to be to distinct locations in the code that you might be stopped in.
+The first place is before the grep starts at all. Here, deparse will show:
+
+      grep { $type eq $_; } 'MSDOS', 'DOS', 'MSWin32', 'Epoc'
+
+But also you might be stopped inside grep. Here deparse will show:
+
+    # code to be run next...
+    $type
+    # contained in...
+    $type eq $_
+
+Knowing which of these two locations can be helpful. For example if you are stopped in the latter location, you can evaluate `$_` to see where in the loop you are at.
+
+See L<Exact Perl location with B::Deparse (and Devel::Callsite)|http://blogs.perl.org/users/rockyb/2015/11/exact-perl-location-with-bdeparse-and-develcallsite.html>.
+
+=head2 deparse
+
+Deparses Perl from interpreter OPs. See L<C<deparse>|Devel::Trepan::CmdProcessor::Command::Deparse> for more information and command syntax.
+
+=head2 deval
+
+This is somewhat like
+L<C<eval>|Devel::Trepan::CmdProcessor::Command::Eval> or C<eval?> which
+evaluates the Perl code that is about to be run, but (when it works),
+it can be more reliable. Eval works on simple-minded string
+manipulation via regular expressions to pull out what to evaluate, whereas C<deval> gets its
+information directly from the interpreter code.
+
+See L<C<deval>|Devel::Trepan::CmdProcessor::Command::Deval> for more information and command syntax.
 
 =head1 AUTHORS
 
